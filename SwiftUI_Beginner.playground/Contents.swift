ac53f904e9bb17aa29440366c33562a57b23c44b
Bhavesh <bhavesh.gupta@techtreeit.com>
2021-08-31 18:43:09 +0530

    created project of SwiftUI Beginner

--- SwiftUI_Beginner.playground/Contents.swift
+++ SwiftUI_Beginner.playground/Contents.swift
@@ -1,3 +1,43 @@
-import UIKit
+import SwiftUI
 
-var str = "Hello, playground"
+/*:
+ ## Property Wrappers
+ */
+//
+//var _property1: String
+//var firstName: String {
+//    get { return _property1.uppercased()}
+//    set { _property1 = newValue}
+//}
+//
+//var _property2: String
+//var secondName: String{
+//    get { return _property2.uppercased()}
+//    set { _property2 = newValue }
+//}
+
+
+@propertyWrapper
+struct Capitalized {
+    private var value: String
+    init(wrappedValue value: String) {
+        self.value = value.uppercased()
+    }
+
+    var wrappedValue: String {
+        get { value }
+        set { value = newValue.uppercased() }
+    }
+}
+
+struct User {
+
+    @Capitalized
+    var firstName: String
+
+    @Capitalized
+    var lastName: String
+}
+
+let user = User(firstName: "Bhavesh", lastName: "Gupta")
+print(user.lastName, user.firstName)
---
+++ SwiftUI_Elements/SwiftUI_Elements/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftUI_Elements
+//
+//  Created by bhavesh on 31/08/21.
+//  Copyright © 2021 Bhavesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
